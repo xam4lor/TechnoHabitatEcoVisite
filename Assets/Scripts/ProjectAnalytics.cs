2298273a7c627329b83111becf0669bb9ff5a5ef
xam4lor <xam4lor@gmail.com>
2016-04-12 21:24:09 +0200

    Correction des animations

---
+++ Assets/Scripts/ProjectAnalytics.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class ProjectAnalytics : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Analytics.CustomEvent("computerInfos", new Dictionary<string, object> {
+			{"device_model", SystemInfo.deviceModel},
+			{"device_name", SystemInfo.deviceName},
+			{"device_type", SystemInfo.deviceType},
+			{"device_unique_identifier", SystemInfo.deviceUniqueIdentifier},
+			{"graphics_memory_size", SystemInfo.graphicsMemorySize},
+			{"operating_system", SystemInfo.operatingSystem},
+			{"processor_count", SystemInfo.processorCount},
+			{"processor_frequency", SystemInfo.processorFrequency},
+			{"processor_type", SystemInfo.processorType},
+			{"system_memory_size", SystemInfo.systemMemorySize}
+		});
+	}
+}
